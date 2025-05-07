724a77ef47be76101f550211f5a86b2acc257b35
Igor Gasparin de Vargas <133717907+igovargas@users.noreply.github.com>
2025-05-06 21:33:01 -0300

    Allow input of multiple comma-separated names up to a limit of 5
    - Implemented input parsing using split() and trim();
    - Allowed user to enter multiple names at once separated by commas;
    - Prevented empty entries and limited the list to exactly 5 names;
    - Sorted the final list alphabetically.

--- src/NameInsert.java
+++ src/NameInsert.java
@@ -13,9 +13,19 @@ public class NameInsert {
         List<String> list = new ArrayList<>();
 
         while (list.size() < 5) {
-            System.out.println("\nEnter a name");
-            String enteredName = input.nextLine();
-            list.add(enteredName);
+            System.out.println("\nWrite 5 names, each separated by a comma.");
+            String enteredNames = input.nextLine();
+            
+            //Criei um Array para armazenar os nome digitados.
+            String[] splitNames = enteredNames.split(",");
+
+            for (String name : splitNames) { //Aqui, o parâmetro (String name : splitNames) vai ver cada nome que foi colocado separado por vírgula lá no Array splitNames(linha 18). Além disso, o comando for vai fazer com que cada nome separado por vírgula seja armazenado na variável "name".    
+                String trimmed = name.trim(); //O método trim() vai remoevr os espaços em branco no início e no final de cada palavra. A variável "trimmed" vai armazenar essa informação para cada nome.
+                if (!trimmed.isEmpty()) { //Com o comando isEmpty, o certo seria informar que "está vazio", porém colocando o "!" no início, adicionamos um "não" ao sentido, ou seja, "se "trimmed" não estiver vazio, faça isso".
+                    list.add(trimmed); //O "faça isso" é adicionar o nome a lista.
+                }
+                if (list.size() == 5) break; //Se a lista chegar a 5 nomes, ela irá parar, por mais que a pessoa tenha adicionado mais nomes do que isso.
+            }  
         }
 
         Collections.sort(list);
