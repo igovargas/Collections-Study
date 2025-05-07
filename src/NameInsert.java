66057e5884bafa4abe1b01b02856673c9140bd3c
Igor Gasparin de Vargas <133717907+igovargas@users.noreply.github.com>
2025-05-06 21:16:35 -0300

    Implement name input and sorting logic
    - Added simpleNameListInput method to collect 5 names from user input;
    - Used Scanner to read input and store names in an ArrayList;
    - Sorted the list alphabetically using Collections.sort();
    - Displayed the final sorted list;
    - Renamed PrincipalList method to main in NameList class to fix runtime error;
    - Added NameInsert.simpleNameListInput() on main class.

--- src/NameInsert.java
+++ src/NameInsert.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class NameInsert {
 
     public static void simpleNameListInput() {
+
+        System.out.println("Let's make a 5 name list!");
         
+        Scanner input = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+
+        while (list.size() < 5) {
+            System.out.println("\nEnter a name");
+            String enteredName = input.nextLine();
+            list.add(enteredName);
+        }
+
+        Collections.sort(list);
+        System.out.println(list);
+
+        input.close();
     }
 }
--- src/NameList.java
+++ src/NameList.java
@@ -1,6 +1,7 @@
 public class NameList {
 
-    public static void PrincipalList() {
+    public static void main(String args[]) {
+        NameInsert.simpleNameListInput();
         
     }
 }
